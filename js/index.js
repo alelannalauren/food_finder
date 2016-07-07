b66d76a1277ad75c985e47b01c0d644d0a205876
RF-code <rfan23@punaou.edu>
2016-07-06 14:15:29 -1000

    Did all the buttons

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
-var display = function(){
+var display1 = function(){
+	document.getElementById('produce').style.backgroundColor = "#01DF3A";
+}
+
+var display2 = function(){
 	document.getElementById('meat').style.backgroundColor = "red";
+}
+
+var display3 = function(){
+	document.getElementById('dairy').style.backgroundColor = "#F78181";
+}
+
+var display4 = function(){
+	document.getElementById('frozen').style.backgroundColor = "#58ACFA";
+}
+
+var display5 = function(){
+	document.getElementById('non-perishable goods').style.backgroundColor = "#FFFF00";
+}
+
+var display6 = function(){
+	document.getElementById('seafood').style.backgroundColor = "#9A2EFE";
 }
