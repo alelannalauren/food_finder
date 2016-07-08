4553da4c5c9dbb0c15ff41d683a9a90707846838
RF-code <rfan23@punaou.edu>
2016-07-07 14:06:28 -1000

    made the colors go away after you click another button

--- js/index.js
+++ js/index.js
@@ -1,23 +1,42 @@
 var display1 = function(){
+	clear();
 	document.getElementById('produce').style.backgroundColor = "#01DF3A";
 }
 
 var display2 = function(){
+	clear();
 	document.getElementById('meat').style.backgroundColor = "red";
 }
 
 var display3 = function(){
+	clear();
 	document.getElementById('dairy').style.backgroundColor = "#F78181";
 }
 
 var display4 = function(){
+	clear();
 	document.getElementById('frozen').style.backgroundColor = "#FF8000";
 }
 
 var display5 = function(){
+	clear();
 	document.getElementById('non-perishable goods').style.backgroundColor = "#FFFF00";
 }
 
 var display6 = function(){
+	clear();
 	document.getElementById('seafood').style.backgroundColor = "#58ACFA";
 }
+
+var clear = function(){
+	for (var i = 0; i <= 5; i++) {
+		document.getElementsByClassName('section')[i].style.backgroundColor = "gray";
+	};
+};
+
+	
+// var dot = function(){
+// 	if (var display1) {
+
+// 	}
+// }
